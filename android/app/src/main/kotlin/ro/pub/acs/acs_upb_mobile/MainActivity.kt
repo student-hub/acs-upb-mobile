2a78008a9503096c6c993d022772c3226d564bc8
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2020-02-10 16:07:27 +0200

    initial commit

---
+++ android/app/src/main/kotlin/ro/pub/acs/acs_upb_mobile/MainActivity.kt
@@ -0,0 +1,12 @@
+package ro.pub.acs.acs_upb_mobile
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
