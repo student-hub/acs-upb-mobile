6683136accf55f7b42098d947588ddde3dcb205c
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2020-02-12 17:29:32 +0200

    test drawer

---
+++ test/drawer_test.dart
@@ -0,0 +1,49 @@
+import 'package:acs_upb_mobile/main.dart';
+import 'package:acs_upb_mobile/module/home/home_page.dart';
+import 'package:acs_upb_mobile/module/settings/settings_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:preferences/preferences.dart';
+
+void main() {
+  group('Drawer', () {
+    setUpAll(() async {
+      WidgetsFlutterBinding.ensureInitialized();
+      PrefService.enableCaching();
+      PrefService.cache = {};
+      PrefService.setString('language', 'en');
+    });
+
+    testWidgets('Home', (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+      await tester.pumpAndSettle();
+
+      expect(find.byType(HomePage), findsOneWidget);
+
+      // Open navigation drawer
+      await tester.tap(find.byIcon(Icons.menu));
+      await tester.pumpAndSettle();
+
+      // Open home
+      await tester.tap(find.byIcon(Icons.home));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(HomePage), findsOneWidget);
+    });
+
+    testWidgets('Settings', (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+      await tester.pumpAndSettle();
+
+      // Open navigation drawer
+      await tester.tap(find.byIcon(Icons.menu));
+      await tester.pumpAndSettle();
+
+      // Open settings
+      await tester.tap(find.text('Settings'));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(SettingsPage), findsOneWidget);
+    });
+  });
+}
