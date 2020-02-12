e1cb8d5c22a9e2e1d660264d8ff0ab8d7642df82
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2020-02-12 18:52:54 +0200

    test language switching

--- test/settings_test.dart
+++ test/settings_test.dart
@@ -10,7 +10,8 @@ void main() {
       WidgetsFlutterBinding.ensureInitialized();
       PrefService.enableCaching();
       PrefService.cache = {};
-      SharedPreferences.setMockInitialValues({'language': 'en'});
+      // Assuming mock system language is English
+      SharedPreferences.setMockInitialValues({'language': 'auto'});
     });
 
     testWidgets('Dark mode', (WidgetTester tester) async {
@@ -42,6 +43,69 @@ void main() {
 
       app = find.byType(MaterialApp).evaluate().first.widget;
       expect(app.theme.brightness, equals(Brightness.dark));
+    });
+
+    testWidgets('Language', (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+      await tester.pumpAndSettle();
+
+      MaterialApp app = find
+          .byType(MaterialApp)
+          .evaluate()
+          .first
+          .widget;
+      expect(app.theme.brightness, equals(Brightness.dark));
+
+      // Open navigation drawer
+      await tester.tap(find.byIcon(Icons.menu));
+      await tester.pumpAndSettle();
+
+      // Open settings
+      await tester.tap(find.text('Settings'));
+      await tester.pumpAndSettle();
+
+      expect(find.text("Auto"), findsOneWidget);
+
+      // Romanian
+      await tester.tap(find.text('Language'));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('Romanian'));
+      await tester.pumpAndSettle();
+
+      // Tap aside to close popup
+      // TODO: Make popup close automatically when choosing an option
+      await tester.tapAt(Offset(1, 1));
+      await tester.pumpAndSettle();
+
+      expect(find.text("Setări"), findsOneWidget);
+      expect(find.text("Română"), findsOneWidget);
+
+      // English
+      await tester.tap(find.text('Limbă'));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('Engleză'));
+      await tester.pumpAndSettle();
+
+      await tester.tapAt(Offset(1, 1));
+      await tester.pumpAndSettle();
+
+      expect(find.text("Settings"), findsOneWidget);
+      expect(find.text("English"), findsOneWidget);
+
+      // Back to Auto (English)
+      await tester.tap(find.text('Language'));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('Auto'));
+      await tester.pumpAndSettle();
+
+      await tester.tapAt(Offset(1, 1));
+      await tester.pumpAndSettle();
+
+      expect(find.text("Settings"), findsOneWidget);
+      expect(find.text("Auto"), findsOneWidget);
     });
   });
 }
