2417d1e6bfbe42d832b083f899ca482dd195c18c
IoanaAlexandru <ioanaa.alexandru98@gmail.com>
2020-02-13 18:43:43 +0200

    first Mac setup
    install CocoaPods and set up project for Mac
    
    everything so far seems to work correctly

---
+++ android/app/src/profile/gen/ro/pub/acs/acs_upb_mobile/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package ro.pub.acs.acs_upb_mobile;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
