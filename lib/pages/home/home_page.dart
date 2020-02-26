7ccb379d9bce334d8a04c496de717cfe07e68faa
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2020-02-26 22:48:55 +0200

    update HomePage to correctly show display name

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,21 +1,21 @@
+import 'package:acs_upb_mobile/authentication/auth_provider.dart';
 import 'package:acs_upb_mobile/generated/l10n.dart';
 import 'package:acs_upb_mobile/widgets/scaffold.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   static const String routeName = '/home';
-  final FirebaseUser user;
-
-  HomePage({this.user});
 
   @override
   Widget build(BuildContext context) {
+    AuthProvider authProvider = Provider.of(context);
+
     return AppScaffold(
         title: S.of(context).drawerHeaderHome,
         body: Center(
-            child: Text(user != null
-                ? S.of(context).welcomeName(user.displayName)
-                : S.of(context).welcomeSimple)));
+            child: Text(authProvider.isAnonymous
+                ? S.of(context).welcomeSimple
+                : S.of(context).welcomeName(authProvider.user.displayName))));
   }
 }
