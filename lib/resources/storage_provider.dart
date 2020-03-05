d38c72654dd5e4cd9c2edabb8f466c85848b4ad6
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2020-03-05 14:57:02 +0200

    cache network images

--- lib/resources/storage_provider.dart
+++ lib/resources/storage_provider.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,6 @@ class Storage {
   static Future<ImageProvider<dynamic>> getImageFromPath(String path) async {
     StorageReference ref = FirebaseStorage.instance.ref().child(path);
     String url = await ref.getDownloadURL();
-    return NetworkImage(url);
+    return CachedNetworkImageProvider(url);
   }
 }
