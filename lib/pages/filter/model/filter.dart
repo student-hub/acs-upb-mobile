b8b70f21be1594924d709436e2f9097689fbbdab
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2020-03-11 15:48:27 +0200

    add methods to manipulate filter data

--- lib/pages/filter/model/filter.dart
+++ lib/pages/filter/model/filter.dart
@@ -1,6 +1,6 @@
 /// Filter represented in the form of a tree with named levels
 ///
-///                                 All
+///                                  All
 ///                    _______________|_______________
 ///                  /                                \
 ///                BSc                               MSc       // Degree
@@ -9,23 +9,63 @@
 ///      IS                 CTI            IA   SPRC ...       // Specialization
 ///   ...|...          ______|______       ⋮      ⋮
 ///                  /    |     |   \
-///                  1    2    3    4                          // Year
+///               CTI-1 CTI-2 CTI-3 CTI-4                      // Year
 ///                  ⋮    ⋮   __|... ⋮
 ///                        /   |
-///                       CA  CB ...                           // Series
+///                     3-CA 3-CB ...                          // Series
 ///                     __|...
 ///                   /   |
-///                 331  332 ...                               // Group
+///               331CA 332CA ...                              // Group
 class Filter {
-  /// Tree structure
+  /// Tree structure for filter.
+  ///
+  /// **Note:** No two nodes should have the same name.
   FilterNode root;
 
-  /// Name of each level of the tree
+  /// Name of each level of the tree.
   ///
   /// **Note:** There should be at least as many names as there are levels in the tree.
   List<Map<String, String>> localizedLevelNames;
 
   Filter({this.root, this.localizedLevelNames});
+
+  void _relevantNodesHelper(List<String> list, FilterNode node) {
+    if (node.children != null) {
+      node.children.forEach((child) => this._relevantNodesHelper(list, child));
+    }
+    if (node.value) {
+      list.add(node.name);
+    }
+  }
+
+  /// Get the names of all nodes with `value = true`.
+  List<String> get relevantNodes {
+    List<String> list = [];
+    _relevantNodesHelper(list, root);
+    return list;
+  }
+
+  bool _setRelevantHelper(String nodeName, FilterNode node) {
+    if (node.name == nodeName) {
+      node.value = true;
+      return true;
+    }
+
+    bool found = false;
+    if (node.children != null) {
+      node.children
+          .forEach((child) => found |= _setRelevantHelper(nodeName, child));
+    }
+
+    if (found) {
+      node.value = true;
+    }
+    return found;
+  }
+
+  bool setRelevant(String nodeName) {
+    return _setRelevantHelper(nodeName, root);
+  }
 }
 
 class FilterNode {
