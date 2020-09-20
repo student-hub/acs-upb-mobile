7554c0633326a64aaaeb564f14974ba1d1a063c1
Octavian <craciunoctavian99@yahoo.com>
2020-09-20 13:47:53 +0300

    Added Intl translations

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,3 +1,4 @@
+import 'package:acs_upb_mobile/generated/l10n.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -46,7 +47,7 @@ class _SearchBarState extends State<SearchBar> {
             child: Container(
               color: Colors.transparent,
               child: Center(
-                child: Text('Cancel'),
+                child: Text(S.of(context).buttonCancel),
               ),
             ),
           ),
