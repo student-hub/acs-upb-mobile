afc06b5c4674a40e009e60c97891547e6564f22d
GeorgeMD <pgn.george@gmail.com>
2020-09-30 22:18:47 +0300

    news feed

---
+++ lib/pages/news_feed/view/news_feed_page.dart
@@ -0,0 +1,43 @@
+import 'package:acs_upb_mobile/pages/news_feed/model/news_feed_item.dart';
+import 'package:acs_upb_mobile/pages/news_feed/service/news_feed_provider.dart';
+import 'package:acs_upb_mobile/resources/utils.dart';
+import 'package:acs_upb_mobile/widgets/scaffold.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class NewsFeedPage extends StatelessWidget {
+  static const String routeName = '/news_feed';
+
+  @override
+  Widget build(BuildContext context) {
+    final newsFeedProvider = Provider.of<NewsFeedProvider>(context);
+
+    return AppScaffold(
+      title: 'News feed',
+      body: FutureBuilder(
+        future: newsFeedProvider.fetchNewsFeedItems(
+            context: context, limit: null),
+        builder: (_, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final List<NewsFeedItem> newsFeedItems = snapshot.data;
+          return ListView(
+              children: ListTile.divideTiles(
+                context: context,
+                tiles: newsFeedItems.map((item) =>
+                    ListTile(
+                      title: Text(item.title),
+                      subtitle: Text(item.date),
+                      onTap: () => Utils.launchURL(item.link, context: context),
+                      dense: true,
+                    )).toList(),
+              ).toList()
+          );
+        },
+      ),
+    );
+  }
+}
