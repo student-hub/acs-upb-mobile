8363ba204f200e4606b61586fa88fd437fb81a10
gabrielovici <silviu.gavriluta98@stud.acs.upb.ro>
2020-10-03 01:09:06 +0300

    reformat files

--- lib/resources/storage/mobile_storage.dart
+++ lib/resources/storage/mobile_storage.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 
-class StorageProvider{
+class StorageProvider {
   static Future<dynamic> findImageUrl(
       BuildContext context, String image) async {
     final String url =
