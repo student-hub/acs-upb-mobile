dca8e8ef4adbc8d2e483e953cb46feb4311b19c5
GeorgeMD <pgn.george@gmail.com>
2020-10-04 19:34:07 +0300

    Comment on why we need the CORS fix

--- lib/pages/news_feed/service/news_provider.dart
+++ lib/pages/news_feed/service/news_provider.dart
@@ -15,6 +15,17 @@ class NewsProvider with ChangeNotifier {
   Future<List<NewsFeedItem>> fetchNewsFeedItems(
       {BuildContext context, int limit}) async {
     try {
+      // The internet is a scary place. CORS (cross origin resource sharing)
+      // prevents websites from accessing resources outside the server that
+      // hosts it. Unless the server we access returns a special header, letting
+      // the browser know that it's ok to access its resources from somewhere
+      // else. In our case we can't modify the acs.pub.ro server to return this
+      // header, so we need to use a proxy. This proxy makes the request for us
+      // and returns the needed header and the content. Utils.wrapUrlWithCORS
+      // prepends the URL of the proxy to the wanted URL so that any request
+      // will go through the proxy. This is needed only for the web version,
+      // as CORS is a web browser thing.
+      // See more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
       final url = kIsWeb
           ? Utils.wrapUrlWithCORS('https://acs.pub.ro')
           : 'https://acs.pub.ro';
