2288cbd85d35b4c37baa6d4462ca7e7ef8810c13
Andrei-Constantin Mirciu <constantinandrei.mirciu@gmail.com>
2020-10-18 18:11:04 +0300

    Add search box on People page

--- lib/pages/people/view/people_page.dart
+++ lib/pages/people/view/people_page.dart
@@ -3,6 +3,8 @@ import 'package:acs_upb_mobile/pages/people/model/person.dart';
 import 'package:acs_upb_mobile/pages/people/service/person_provider.dart';
 import 'package:acs_upb_mobile/pages/people/view/person_view.dart';
 import 'package:acs_upb_mobile/widgets/scaffold.dart';
+import 'package:acs_upb_mobile/widgets/search_bar.dart';
+import 'package:dynamic_text_highlighting/dynamic_text_highlighting.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,7 +17,11 @@ class PeoplePage extends StatefulWidget {
 }
 
 class _PeoplePageState extends State<PeoplePage> {
+  String filter = '';
+  bool searchClosed = true;
   Future<List<Person>> people;
+  List<Person> peopleData;
+  bool visibilitySearchBar = false;
 
   @override
   void initState() {
@@ -27,30 +33,108 @@ class _PeoplePageState extends State<PeoplePage> {
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
+      actions: [
+        AppScaffoldAction(
+          icon: Icons.search,
+          onPressed: () {
+            setState(() {
+              searchClosed = !searchClosed;
+              visibilitySearchBar = !visibilitySearchBar;
+            });
+          },
+        )
+      ],
       title: Text(S.of(context).navigationPeople),
       body: Container(
         child: FutureBuilder(
             future: people,
             builder: (_, snapshot) {
               if (snapshot.connectionState == ConnectionState.done) {
-                final List<Person> peopleData = snapshot.data;
-                return ListView.builder(
-                    itemCount: peopleData.length,
-                    itemBuilder: (_, index) {
-                      return ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage:
-                              NetworkImage(peopleData[index].photo),
-                        ),
-                        title: Text(peopleData[index].name),
-                        subtitle: Text(peopleData[index].email),
-                        onTap: () => showPersonInfo(peopleData[index]),
-                      );
-                    });
+                peopleData = snapshot.data;
+                return ListView(
+                  children: [
+                    Visibility(
+                      visible: visibilitySearchBar,
+                      child: SearchWidget(
+                        onSearch: (searchText) {
+                          setState(() {
+                            filter = searchText;
+                          });
+                        },
+                        cancelCallback: () {
+                          setState(() {
+                            searchClosed = true;
+                            visibilitySearchBar = false;
+                            filter = '';
+                          });
+                        },
+                        searchClosed: searchClosed,
+                      ),
+                    ),
+                    PeopleList(people: filteredPeople, filter: filter)
+                  ],
+                );
               } else {
                 return const Center(child: CircularProgressIndicator());
               }
             }),
+      ),
+    );
+  }
+
+  List<Person> get filteredPeople => peopleData
+      .where((person) => filter
+          .split(' ')
+          .where((element) => element != '')
+          .fold(
+              true,
+              (previousValue, filter) =>
+                  previousValue && person.name.toLowerCase().contains(filter)))
+      .toList();
+}
+
+class PeopleList extends StatefulWidget {
+  const PeopleList({this.people, this.filter});
+
+  final List<Person> people;
+  final String filter;
+
+  @override
+  _PeopleListState createState() => _PeopleListState();
+}
+
+class _PeopleListState extends State<PeopleList> {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> filteredWords =
+        widget.filter.split(' ').where((element) => element != '').toList();
+    return Padding(
+      padding: const EdgeInsets.only(top: 12),
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: widget.people.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            key: ValueKey(widget.people[index].name),
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(widget.people[index].photo),
+            ),
+            title: filteredWords.isNotEmpty
+                ? DynamicTextHighlighting(
+                    text: widget.people[index].name,
+                    style: Theme.of(context).textTheme.subtitle1,
+                    highlights: filteredWords,
+                    color: Theme.of(context).accentColor,
+                    caseSensitive: false,
+                  )
+                : Text(
+                    widget.people[index].name,
+                  ),
+            subtitle: Text(widget.people[index].email),
+            onTap: () => showPersonInfo(widget.people[index]),
+          );
+        },
       ),
     );
   }
