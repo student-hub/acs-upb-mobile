04816860ab3f11024ee247542476893fe5c217a3
Adrian Margineanu <a.margineanu98@gmail.com>
2020-10-24 23:41:37 +0300

    Correct the failing tests

--- lib/authentication/view/edit_profile_page.dart
+++ lib/authentication/view/edit_profile_page.dart
@@ -44,9 +44,8 @@ class _EditProfilePageState extends State<EditProfilePage> {
   void initState() {
     super.initState();
     final authProvider = Provider.of<AuthProvider>(context, listen: false);
-    authProvider
-        .getProfilePictureURL(context: context)
-        .then((value) => setState(() => imageWidget = NetworkImage(value)));
+    authProvider.getProfilePictureURL(context: context).then((value) =>
+        setState(() => {if (value == null) imageWidget = NetworkImage(value)}));
   }
 
   AppDialog _changePasswordDialog(BuildContext context) {
