7df8a60f86be48efd1b1a22480cc8531cf584ef7
Maria Stoichitescu <stoichitescumaria@yahoo.com>
2021-03-06 18:18:53 +0200

    Diacritics added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:acs_upb_mobile/authentication/service/auth_provider.dart';
 import 'package:acs_upb_mobile/authentication/view/login_view.dart';
 import 'package:acs_upb_mobile/authentication/view/sign_up_view.dart';
