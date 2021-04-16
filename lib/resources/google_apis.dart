ce0ca56059dd29260a360a1365a534d32fb576a3
Bogdan Piele <bogdanpielemail@gmail.com>
2021-04-16 03:36:35 +0300

    Organized GCal code
    Moved Google Calendar color details to google_apis.dart.
    Created getter which assigns an event type to a Google Calendar color (which has a color ID and a name). This is done manually, with regards to how event type colors are assigned in the ACS UPB Mobile Timetable.

--- lib/resources/google_apis.dart
+++ lib/resources/google_apis.dart
@@ -1,4 +1,5 @@
 import 'dart:io' show Platform;
+import 'package:acs_upb_mobile/pages/timetable/model/events/uni_event.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:googleapis/calendar/v3.dart';
@@ -27,5 +28,57 @@ class GoogleApiHelper {
 
     return ClientId(_clientIdString, '');
   }
+}
+enum GoogleCalendarColorNames {
+  undefined,
+  lavender,
+  sage,
+  grape,
+  flamingo,
+  banana,
+  tangerine,
+  peacock,
+  graphite,
+  blueberry,
+  basil,
+  tomato
+}
 
+enum GoogleCalendarColorHexVaues {
+  undefined,
+  HEX7986cb,
+  HEX33b679,
+  HEX8e24aa,
+  HEXe67c73,
+  HEXf6c026,
+  HEXf5511d,
+  HEX039be5,
+  HEX616161,
+  HEX3f51b5,
+  HEX0b8043,
+  HEXd60000
+}
+
+
+extension UniEventTypeGCalColor on UniEventType{
+  GoogleCalendarColorNames get googleCalendarColor {
+    switch (this) {
+      case UniEventType.lecture:
+        return GoogleCalendarColorNames.flamingo;
+      case UniEventType.lab:
+        return GoogleCalendarColorNames.peacock ;
+      case UniEventType.seminar:
+        return GoogleCalendarColorNames.tangerine;
+      case UniEventType.sports:
+        return GoogleCalendarColorNames.basil;
+      case UniEventType.semester:
+        return GoogleCalendarColorNames.undefined;
+      case UniEventType.holiday:
+        return GoogleCalendarColorNames.banana;
+      case UniEventType.examSession:
+        return GoogleCalendarColorNames.tomato;
+      default:
+        return GoogleCalendarColorNames.undefined;
+    }
+  }
 }
