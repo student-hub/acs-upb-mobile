36086905d4bb310157dc90e688bbbd5d68ff896a
Gabriel Gavriluta <gavriluta.gabriel@gmail.com>
2021-05-29 23:00:36 +0300

    Make code more modular

---
+++ lib/pages/search/view/classes_search_results.dart
@@ -0,0 +1,110 @@
+import 'package:acs_upb_mobile/pages/classes/model/class.dart';
+import 'package:acs_upb_mobile/pages/classes/service/class_provider.dart';
+import 'package:acs_upb_mobile/pages/classes/view/class_view.dart';
+import 'package:acs_upb_mobile/pages/search/view/seached_classes_view.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:acs_upb_mobile/generated/l10n.dart';
+
+class ClassesSearchResults extends StatelessWidget {
+  const ClassesSearchResults({this.query});
+
+  final String query;
+
+  @override
+  Widget build(BuildContext context) {
+    List<ClassHeader> classesSearched;
+    return FutureBuilder(
+        future:
+            Provider.of<ClassProvider>(context, listen: false).search(query),
+        builder: (_, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              query.isNotEmpty) {
+            classesSearched = snapshot.data;
+            if (classesSearched.isNotEmpty) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                      child: Row(
+                        children: [
+                          Text(S.current.labelClasses,
+                              style: const TextStyle(fontSize: 15)),
+                          Expanded(
+                              child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute<SearchedClassesView>(
+                                      builder: (_) => SearchedClassesView(
+                                            classHeaders: classesSearched,
+                                            query: query,
+                                          )));
+                            },
+                            child: Text(
+                              S.current.actionShowMore,
+                              textAlign: TextAlign.right,
+                              style: const TextStyle(
+                                  fontSize: 15, color: Colors.blue),
+                            ),
+                          ))
+                        ],
+                      )),
+                  ClassesCircleList(classHeaders: classesSearched)
+                ],
+              );
+            }
+          }
+          return Container();
+        });
+  }
+}
+
+class ClassesCircleList extends StatelessWidget {
+  const ClassesCircleList({this.classHeaders});
+
+  final List<ClassHeader> classHeaders;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+            children: classHeaders
+                .take(3)
+                .map((classHeader) => Container(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),
+                    child: GestureDetector(
+                        child: Row(children: <Widget>[
+                          Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 10, 10),
+                              child: CircleAvatar(
+                                backgroundColor: Colors.grey,
+                                child: Container(
+                                  width: 30,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: AutoSizeText(
+                                      classHeader.acronym,
+                                      minFontSize: 0,
+                                      maxLines: 1,
+                                    ),
+                                  ),
+                                ),
+                              )),
+                          Expanded(
+                            child: Text(classHeader.name),
+                          )
+                        ]),
+                        onTap: () => {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute<ClassView>(
+                                builder: (_) =>
+                                    ClassView(classHeader: classHeader),
+                              ))
+                            })))
+                .toList()));
+  }
+}
--- lib/pages/search/view/search_page.dart
+++ lib/pages/search/view/search_page.dart
@@ -1,20 +1,11 @@
-import 'dart:math';
-
-import 'package:acs_upb_mobile/pages/classes/model/class.dart';
-import 'package:acs_upb_mobile/pages/classes/service/class_provider.dart';
-import 'package:acs_upb_mobile/pages/classes/view/class_view.dart';
-import 'package:acs_upb_mobile/pages/people/model/person.dart';
-import 'package:acs_upb_mobile/pages/people/service/person_provider.dart';
-import 'package:acs_upb_mobile/pages/people/view/person_view.dart';
-import 'package:acs_upb_mobile/pages/search/view/seached_classes_view.dart';
+import 'package:acs_upb_mobile/pages/search/view/classes_search_results.dart';
+import 'package:acs_upb_mobile/pages/search/view/people_search_results.dart';
 import 'package:acs_upb_mobile/widgets/scaffold.dart';
 import 'package:acs_upb_mobile/widgets/search_bar.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:acs_upb_mobile/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'package:provider/provider.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -23,9 +14,7 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   String query = '';
-  List<ClassHeader> classesSearched;
   bool searchClosed = true;
-  List<Person> peopleSearched;
 
   @override
   void initState() {
@@ -44,265 +33,43 @@ class _SearchPageState extends State<SearchPage> {
             cancelCallback: () => {setState(() => query = '')},
             searchClosed: false,
           ),
-          if (query.isNotEmpty)
-            FutureBuilder(
-                future: Provider.of<PersonProvider>(context, listen: false)
-                    .search(query),
-                builder: (_, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    peopleSearched = snapshot.data;
-                    if (peopleSearched.isNotEmpty) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(10, 10, 0, 10),
-                            child: Text(
-                              S.current.labelPeople,
-                              style: const TextStyle(fontSize: 15),
-                            ),
-                          ),
-                          PeopleCircleList(
-                            people: peopleSearched,
-                            query: query,
-                          )
-                        ],
-                      );
-                    }
-                  }
-                  return Container();
-                }),
-          const Divider(),
-          if (query.isNotEmpty)
-            FutureBuilder(
-                future: Provider.of<ClassProvider>(context, listen: false)
-                    .search(query),
-                builder: (_, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    classesSearched = snapshot.data;
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        if (classesSearched.isNotEmpty)
-                          Padding(
-                              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-                              child: Row(
-                                children: [
-                                  Text(S.current.labelClasses,
-                                      style: const TextStyle(fontSize: 15)),
-                                  Expanded(
-                                      child: GestureDetector(
-                                    onTap: () {
-                                      Navigator.of(context).push(
-                                          MaterialPageRoute<
-                                                  SearchedClassesView>(
-                                              builder: (_) =>
-                                                  SearchedClassesView(
-                                                    classHeaders:
-                                                        classesSearched,
-                                                    query: query,
-                                                  )));
-                                    },
-                                    child: Text(
-                                      S.current.actionShowMore,
-                                      textAlign: TextAlign.right,
-                                      style: const TextStyle(
-                                          fontSize: 15, color: Colors.blue),
-                                    ),
-                                  ))
-                                ],
-                              )),
-                        ClassesCircleList(
-                            classHeaders: classesSearched, query: query)
-                      ],
-                    );
-                  }
-                  return Container();
-                }),
-          if (query.isNotEmpty)
-            Container(
-                child: Column(
-              children: [
-                const Image(
-                  image:
-                      AssetImage('assets/illustrations/undraw_chat_image.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                          padding: const EdgeInsets.only(right: 10),
-                          child: Text(S.current.messageAnotherQuestion)),
-                      Text(
-                        S.current.messageTalkToChatbot,
-                        style: TextStyle(
-                            color: Theme.of(context).accentColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ))
+          PeopleSearchResults(query: query),
+          ClassesSearchResults(query: query),
+          ChatBotIntro()
         ],
       ),
     );
   }
 }
 
-class PeopleCircleList extends StatelessWidget {
-  const PeopleCircleList({this.people, this.query});
-
-  final List<Person> people;
-  final String query;
-
+class ChatBotIntro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    TextEditingController _labelController;
     return Container(
-      constraints: BoxConstraints.expand(
-        height: Theme.of(context).textTheme.headline4.fontSize * 1.1 + 60.0,
-      ),
-      padding: const EdgeInsets.all(10),
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemCount: people.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (BuildContext context, int index) {
-          final List<String> name = people[index].name.split(' ');
-          _labelController =
-              TextEditingController(text: people[index].name ?? '');
-          return Container(
-              padding: const EdgeInsets.fromLTRB(0, 0, 25, 0),
-              child: GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet<dynamic>(
-                        isScrollControlled: true,
-                        context: context,
-                        builder: (BuildContext buildContext) => PersonView(
-                              person: people[index],
-                            ));
-                  },
-                  child: Container(
-                    width: 60,
-                    height: 60,
-                    child: Column(children: <Widget>[
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(people[index].photo),
-                      ),
-                      AutoSizeText(
-                        people[index].name,
-                        maxLines: 2,
-                        minFontSize: 10,
-                        maxFontSize: 12,
-                        textAlign: TextAlign.center,
-                      )
-                    ]),
-                  )));
-        },
-      ),
-    );
-  }
-}
-
-class ClassesCircleList extends StatelessWidget {
-  const ClassesCircleList({this.classHeaders, this.query});
-
-  final List<ClassHeader> classHeaders;
-  final String query;
-
-  @override
-  Widget build(BuildContext context) {
-    if (classHeaders.isNotEmpty) {
-      final int nr = min(classHeaders.length, 3);
-      return Container(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-              children: classHeaders
-                  .take(3)
-                  .map((classHeader) => Container(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),
-                      child: GestureDetector(
-                          child: Row(children: <Widget>[
-                            Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(0, 0, 10, 10),
-                                child: CircleAvatar(
-                                  backgroundColor: Colors.grey,
-                                  child: Container(
-                                    width: 30,
-                                    child: Align(
-                                      alignment: Alignment.center,
-                                      child: AutoSizeText(
-                                        classHeader.acronym,
-                                        minFontSize: 0,
-                                        maxLines: 1,
-                                      ),
-                                    ),
-                                  ),
-                                )),
-                            Expanded(
-                              child: Text(classHeader.name),
-                            )
-                          ]),
-                          onTap: () => {
-                                Navigator.of(context)
-                                    .push(MaterialPageRoute<ClassView>(
-                                  builder: (_) =>
-                                      ClassView(classHeader: classHeader),
-                                ))
-                              })))
-                  .toList())
-//      child: ListView.builder(
-//        shrinkWrap: true,
-//        itemCount: classHeaders.length > 5 ? 5 : classHeaders.length,
-//        scrollDirection: Axis.vertical,
-//        itemBuilder: (BuildContext context, int index) {
-//          return Container(
-//            padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),
-//            child: GestureDetector(
-//              child: Row(children: <Widget>[
-//                 Padding(
-//                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 10),
-//                  child: CircleAvatar(
-//                    backgroundColor: Colors.grey,
-//                    child: Container(
-//                      width: 30,
-//                      child: Align(
-//                        alignment: Alignment.center,
-//                        child: AutoSizeText(
-//                          classHeaders[index].acronym,
-//                          minFontSize: 0,
-//                          maxLines: 1,
-//                        ),
-//                      ),
-//                    ),
-//                  )
-//                ),
-//                //TODO Solve pixel overflow
-//                Expanded(
-//                  child: Text(classHeaders[index].name),
-//                )
-//              ]),
-//              onTap: () => {
-//                Navigator.of(context).push(
-//                    MaterialPageRoute<ClassView>(
-//                      builder: (_) => ClassView(
-//                        classHeader: classHeaders[index]
-//                      ),
-//                    )
-//                )
-//              }
-//            )
-//          );
-//        }
-//      ),
-          );
-    } else {
-      return Container();
-    }
+        child: Column(
+      children: [
+        const Image(
+          image: AssetImage('assets/illustrations/undraw_chat_image.png'),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Text(S.current.messageAnotherQuestion)),
+              Text(
+                S.current.messageTalkToChatbot,
+                style: TextStyle(
+                    color: Theme.of(context).accentColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+              )
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
