a52b79de0987cbe14008dc77374333ceacf7b950
Bogdan Piele <bogdanpielemail@gmail.com>
2021-08-26 22:58:43 +0300

    Started to supply recurrent and all-day events asynchronously via eventProvider

--- lib/pages/timetable/model/events/uni_event.dart
+++ lib/pages/timetable/model/events/uni_event.dart
@@ -128,6 +128,16 @@ class UniEvent {
     return generateInstances().first.dateString;
   }
 
+  // UniEventInstance toUniEventInstance() {
+  //   return UniEventInstance(
+  //     title: name,
+  //     mainEvent: this,
+  //     start: start,
+  //     end: start.add(period.toTime().toDuration),
+  //     location: location,
+  //   );
+  // }
+
   Iterable<UniEventInstance> generateInstances(
       {final Interval intersectingInterval}) sync* {
     final DateTime end = start.add(duration);
