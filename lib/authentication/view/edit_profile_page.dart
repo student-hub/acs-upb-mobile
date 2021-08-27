feea90fd6368c8db907bf516e0fc3e9fd38fbbc9
Barbulescu Robert-Gabriel <barbulescurobert@yahoo.com>
2021-08-27 20:41:51 +0300

    first commit

--- lib/authentication/view/edit_profile_page.dart
+++ lib/authentication/view/edit_profile_page.dart
@@ -113,7 +113,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
                   if (value?.isEmpty ?? true) {
                     return S.current.errorNoPassword;
                   }
-                  if (value == newPasswordController.text) {
+                  if (value != newPasswordController.text) {
                     return S.current.errorPasswordsDiffer;
                   }
                   return null;
