47d4193e1ba8428aeaa95d0a7e419a525be3af4f
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2021-09-19 17:08:10 +0200

    Fix imports.

--- lib/pages/settings/view/settings_page.dart
+++ lib/pages/settings/view/settings_page.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:acs_upb_mobile/resources/theme.dart';
 import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
