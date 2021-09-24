2bd2c861c484d4aa1c1a70ec686ab0a8c3119bfe
Razvan Rotaru <razvanrtr@outlook.com>
2021-09-24 20:28:55 +0300

    fix format

--- lib/pages/people/view/person_view.dart
+++ lib/pages/people/view/person_view.dart
@@ -12,7 +12,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:provider/provider.dart';
 
-
 class PersonView extends StatelessWidget {
   const PersonView({Key key, this.person}) : super(key: key);
   final Person person;
