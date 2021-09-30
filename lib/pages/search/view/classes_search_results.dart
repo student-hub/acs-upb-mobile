39cc74d3741fcb5dffca8574072332f37e2d0f20
Razvan Rotaru <razvanrtr@outlook.com>
2021-10-01 02:14:24 +0300

    remove unnecessary param and add authentication checks

--- lib/pages/search/view/classes_search_results.dart
+++ lib/pages/search/view/classes_search_results.dart
@@ -1,9 +1,12 @@
+import 'package:acs_upb_mobile/authentication/service/auth_provider.dart';
 import 'package:acs_upb_mobile/generated/l10n.dart';
+import 'package:acs_upb_mobile/navigation/service/navigator.dart';
 import 'package:acs_upb_mobile/pages/classes/model/class.dart';
 import 'package:acs_upb_mobile/pages/classes/service/class_provider.dart';
 import 'package:acs_upb_mobile/pages/classes/view/class_view.dart';
 import 'package:acs_upb_mobile/pages/search/view/seached_classes_view.dart';
 import 'package:acs_upb_mobile/widgets/class_icon.dart';
+import 'package:acs_upb_mobile/widgets/toast.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -35,22 +38,27 @@ class ClassesSearchResults extends StatelessWidget {
                           Text(S.current.labelClasses,
                               style: const TextStyle(fontSize: 15)),
                           Expanded(
-                              child: GestureDetector(
-                            onTap: () {
-                              Navigator.of(context)
-                                  .push(MaterialPageRoute<SearchedClassesView>(
+                            child: GestureDetector(
+                              onTap: () {
+                                AppNavigator.push(
+                                    context,
+                                    MaterialPageRoute<SearchedClassesView>(
                                       builder: (_) => SearchedClassesView(
-                                            classHeaders: classesSearched,
-                                            query: query,
-                                          )));
-                            },
-                            child: Text(
-                              S.current.actionShowMore,
-                              textAlign: TextAlign.right,
-                              style: const TextStyle(
-                                  fontSize: 15, color: Colors.blue),
+                                        classHeaders: classesSearched,
+                                      ),
+                                    ),
+                                    webPath:
+                                        '${SearchedClassesView.routeName}?ids='
+                                        '${classesSearched.map((classSearched) => '${classSearched.id}').join('&')}');
+                              },
+                              child: Text(
+                                S.current.actionShowMore,
+                                textAlign: TextAlign.right,
+                                style: const TextStyle(
+                                    fontSize: 15, color: Colors.blue),
+                              ),
                             ),
-                          ))
+                          ),
                         ],
                       )),
                   ClassesCircleList(classHeaders: classesSearched)
@@ -71,41 +79,55 @@ class ClassesCircleList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-            children: classHeaders
-                .take(3)
-                .map((classHeader) => Container(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),
-                    child: GestureDetector(
-                        child: Row(children: <Widget>[
-                          Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 0, 10, 10),
-                              child: CircleAvatar(
-                                backgroundColor: classHeader.colorFromAcronym,
-                                child: Container(
-                                  width: 30,
-                                  child: Align(
-                                    alignment: Alignment.center,
-                                    child: AutoSizeText(
-                                      classHeader.acronym,
-                                      minFontSize: 0,
-                                      maxLines: 1,
-                                    ),
-                                  ),
-                                ),
-                              )),
-                          Expanded(
-                            child: Text(classHeader.name),
-                          )
-                        ]),
-                        onTap: () => {
-                              Navigator.of(context)
-                                  .push(MaterialPageRoute<ClassView>(
-                                builder: (_) =>
-                                    ClassView(classHeader: classHeader),
-                              ))
-                            })))
-                .toList()));
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: classHeaders.take(3).map(
+          (classHeader) {
+            return Container(
+              padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),
+              child: GestureDetector(
+                child: Row(children: <Widget>[
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 10, 10),
+                      child: CircleAvatar(
+                        backgroundColor: classHeader.colorFromAcronym,
+                        child: Container(
+                          width: 30,
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: AutoSizeText(
+                              classHeader.acronym,
+                              minFontSize: 0,
+                              maxLines: 1,
+                            ),
+                          ),
+                        ),
+                      )),
+                  Expanded(
+                    child: Text(classHeader.name),
+                  )
+                ]),
+                onTap: () {
+                  final currentUser =
+                      Provider.of<AuthProvider>(context, listen: false)
+                          .currentUserFromCache;
+                  print('CurrentUser: $currentUser');
+                  if (currentUser != null) {
+                    AppNavigator.push(
+                        context,
+                        MaterialPageRoute<ClassView>(
+                          builder: (_) => ClassView(classHeader: classHeader),
+                        ),
+                        webPath: '${ClassView.routeName}?id=${classHeader.id}');
+                  } else {
+                    AppToast.show(S.current.warningAuthenticationNeeded);
+                  }
+                },
+              ),
+            );
+          },
+        ).toList(),
+      ),
+    );
   }
 }
--- lib/pages/search/view/seached_classes_view.dart
+++ lib/pages/search/view/seached_classes_view.dart
@@ -1,17 +1,22 @@
+import 'package:acs_upb_mobile/authentication/service/auth_provider.dart';
+import 'package:acs_upb_mobile/generated/l10n.dart';
+import 'package:acs_upb_mobile/navigation/service/navigator.dart';
 import 'package:acs_upb_mobile/navigation/view/scaffold.dart';
 import 'package:acs_upb_mobile/pages/classes/model/class.dart';
 import 'package:acs_upb_mobile/pages/classes/view/class_view.dart';
+import 'package:acs_upb_mobile/widgets/class_icon.dart';
+import 'package:acs_upb_mobile/widgets/toast.dart';
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:acs_upb_mobile/generated/l10n.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:acs_upb_mobile/widgets/class_icon.dart';
+import 'package:provider/provider.dart';
 
 class SearchedClassesView extends StatelessWidget {
-  const SearchedClassesView({this.classHeaders, this.query});
+  const SearchedClassesView({this.classHeaders});
+
+  static const String routeName = 'classes/search';
 
   final List<ClassHeader> classHeaders;
-  final String query;
 
   @override
   Widget build(BuildContext context) {
@@ -20,45 +25,57 @@ class SearchedClassesView extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.all(10),
         child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: classHeaders.length,
-            scrollDirection: Axis.vertical,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),
-                child: GestureDetector(
-                    child: Row(children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 10, 10),
-                        child: CircleAvatar(
-                          backgroundColor: classHeaders[index].colorFromAcronym,
-                          child: Container(
-                            width: 30,
-                            child: Align(
-                              alignment: Alignment.center,
-                              child: AutoSizeText(
-                                classHeaders[index].acronym,
-                                minFontSize: 0,
-                                maxLines: 1,
-                              ),
-                            ),
+          shrinkWrap: true,
+          itemCount: classHeaders.length,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),
+              child: GestureDetector(
+                child: Row(children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 10, 10),
+                    child: CircleAvatar(
+                      backgroundColor: classHeaders[index].colorFromAcronym,
+                      child: Container(
+                        width: 30,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: AutoSizeText(
+                            classHeaders[index].acronym,
+                            minFontSize: 0,
+                            maxLines: 1,
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: Text(classHeaders[index].name),
-                      )
-                    ]),
-                    onTap: () => {
-                          Navigator.of(context).push(
-                            MaterialPageRoute<ClassView>(
-                              builder: (_) =>
-                                  ClassView(classHeader: classHeaders[index]),
-                            ),
-                          )
-                        }),
-              );
-            }),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(classHeaders[index].name),
+                  )
+                ]),
+                onTap: () {
+                  final currentUser =
+                      Provider.of<AuthProvider>(context, listen: false)
+                          .currentUserFromCache;
+                  print('CurrentUser: $currentUser');
+                  if (currentUser != null) {
+                    AppNavigator.push(
+                        context,
+                        MaterialPageRoute<ClassView>(
+                          builder: (_) =>
+                              ClassView(classHeader: classHeaders[index]),
+                        ),
+                        webPath:
+                            '${ClassView.routeName}?id=${classHeaders[index].id}');
+                  } else {
+                    AppToast.show(S.current.warningAuthenticationNeeded);
+                  }
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
