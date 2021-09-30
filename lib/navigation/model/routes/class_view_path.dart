cd25557f66e3271745c780754625e82ae692362d
Razvan Rotaru <razvanrtr@outlook.com>
2021-10-01 02:17:14 +0300

    add authentication check for ClassView

--- lib/navigation/model/routes/class_view_path.dart
+++ lib/navigation/model/routes/class_view_path.dart
@@ -9,7 +9,16 @@ class ClassViewPath extends RoutePath {
   Widget get page {
     return Builder(
       builder: (BuildContext context) {
+        final authProvider = Provider.of<AuthProvider>(context);
         final classProvider = Provider.of<ClassProvider>(context);
+
+        if (!authProvider.isAuthenticated || authProvider.isAnonymous) {
+          return ErrorPage(
+            errorMessage: S.current.warningAuthenticationNeeded,
+            actionText: S.current.actionLogIn,
+            actionOnTap: () => Utils.signOut(context),
+          );
+        }
 
         return FutureBuilder<ClassHeader>(
           future: classProvider.fetchClassHeader(id),
