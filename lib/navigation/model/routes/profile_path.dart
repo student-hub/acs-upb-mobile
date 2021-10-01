9d4f00a4c1f31d6b7e7a6508a30438df719c3cf5
Razvan Rotaru <razvanrtr@outlook.com>
2021-10-02 02:03:47 +0300

    additional checks on profile_path.dart

--- lib/navigation/model/routes/profile_path.dart
+++ lib/navigation/model/routes/profile_path.dart
@@ -15,7 +15,20 @@ class ProfilePath extends RoutePath {
               .fetchPerson(name),
           builder: (_, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
+              if (!snapshot.hasData) {
+                return ErrorPage(
+                  errorMessage: S.current.labelUnknown,
+                );
+              }
+
               final Person personData = snapshot.data;
+
+              if (personData.office == null || personData.email == null) {
+                return ErrorPage(
+                  errorMessage: S.current.labelUnknown,
+                );
+              }
+
               return PersonView(
                 person: personData,
               );
