0aa23f0f98e68649f5cfbb066aea40d44cdc571f
Adrian <a.margineanu98@gmail.com>
2022-03-14 21:17:53 +0200

    Fix PR comments

--- lib/pages/classes/view/class_view.dart
+++ lib/pages/classes/view/class_view.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 
 import '../../../authentication/service/auth_provider.dart';
 import '../../../generated/l10n.dart';
+import '../../../resources/remote_config.dart';
 import '../../../resources/utils.dart';
 import '../../../widgets/button.dart';
 import '../../../widgets/class_icon.dart';
