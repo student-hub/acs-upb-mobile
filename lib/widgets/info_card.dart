c57c3a1c05973a26df1e8746dd958e6c9fe7dcef
Stefan Popa <andrei.popa2199@gmail.com>
2022-04-20 00:22:00 +0300

    Remove not yet widget overall

--- lib/widgets/info_card.dart
+++ lib/widgets/info_card.dart
@@ -21,89 +21,79 @@ class InfoCard<T> extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-    return Padding(
-      padding: padding ?? const EdgeInsets.fromLTRB(12, 12, 12, 0),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  if (title != null && title.isNotEmpty)
-                    Expanded(
-                      child: Text(
-                        title,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline6
-                            .copyWith(fontSize: 18),
-                        overflow: TextOverflow.fade,
-                        maxLines: 2,
-                      ),
-                    ),
-                  if (onShowMore != null)
-                    GestureDetector(
-                      onTap: onShowMore,
-                      key: showMoreButtonKey ?? const ValueKey('show_more'),
-                      child: Row(
-                        children: <Widget>[
-                          Text(
-                            S.current.actionShowMore,
-                            style: Theme.of(context)
-                                .coloredTextTheme
-                                .subtitle2
-                                .copyWith(
-                                    color: Theme.of(context).primaryColor),
-                          ),
-                          Icon(
-                            Icons.arrow_forward_ios_outlined,
-                            color: Theme.of(context).primaryColor,
-                            size:
-                                Theme.of(context).textTheme.subtitle2.fontSize,
-                          )
-                        ],
-                      ),
-                    ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              FutureBuilder(
-                  future: future,
-                  builder: (final context, final snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      if (snapshot.hasData) {
-                        if ((snapshot.data is Map ||
-                                snapshot.data is Iterable) &&
-                            snapshot.data.isEmpty) {
-                          return noneYet(context);
-                        }
+    return FutureBuilder(
+      future: future,
+        builder: (final context, final snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              if ((snapshot.data is Map ||
+                  snapshot.data is Iterable) &&
+                  snapshot.data.isEmpty) {
+                return SizedBox.shrink();
+              }
 
-                        return builder(snapshot.data);
-                      } else {
-                        return noneYet(context);
-                      }
-                    }
-                    return const SizedBox(
-                      height: 100,
-                      child: Center(child: CircularProgressIndicator()),
-                    );
-                  }),
-            ],
-          ),
-        ),
-      ),
-    );
+              return Padding(
+                padding: padding ?? const EdgeInsets.fromLTRB(12, 12, 12, 0),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            if (title != null && title.isNotEmpty)
+                              Expanded(
+                                child: Text(
+                                  title,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline6
+                                      .copyWith(fontSize: 18),
+                                  overflow: TextOverflow.fade,
+                                  maxLines: 2,
+                                ),
+                              ),
+                            if (onShowMore != null)
+                              GestureDetector(
+                                onTap: onShowMore,
+                                key: showMoreButtonKey ?? const ValueKey('show_more'),
+                                child: Row(
+                                  children: <Widget>[
+                                    Text(
+                                      S.current.actionShowMore,
+                                      style: Theme.of(context)
+                                          .coloredTextTheme
+                                          .subtitle2
+                                          .copyWith(
+                                          color: Theme.of(context).primaryColor),
+                                    ),
+                                    Icon(
+                                      Icons.arrow_forward_ios_outlined,
+                                      color: Theme.of(context).primaryColor,
+                                      size:
+                                      Theme.of(context).textTheme.subtitle2.fontSize,
+                                    )
+                                  ],
+                                ),
+                              ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        builder(snapshot.data),
+                      ]
+                    )
+                  )
+                )
+              );
+            } else {
+              return SizedBox.shrink();
+            }
+          }
+          return const SizedBox(
+            height: 100,
+            child: Center(child: CircularProgressIndicator()),
+          );
+        });
   }
-
-  Widget noneYet(final BuildContext context) => SizedBox(
-        height: 100,
-        child: Center(
-          child: Text(
-            S.current.warningNoneYet,
-            style: TextStyle(color: Theme.of(context).disabledColor),
-          ),
-        ),
-      );
 }
