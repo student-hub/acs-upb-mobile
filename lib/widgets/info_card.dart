504ee8d7dbc6b987133eb5dcb356576720530854
Stefan Popa <andrei.popa2199@gmail.com>
2022-04-27 14:57:12 +0300

    Formatted file

--- lib/widgets/info_card.dart
+++ lib/widgets/info_card.dart
@@ -6,12 +6,12 @@ import '../resources/theme.dart';
 class InfoCard<T> extends StatelessWidget {
   const InfoCard(
       {@required this.future,
-        @required this.builder,
-        this.onShowMore,
-        this.showIfEmpty = false,
-        this.title,
-        this.showMoreButtonKey,
-        this.padding});
+      @required this.builder,
+      this.onShowMore,
+      this.showIfEmpty = false,
+      this.title,
+      this.showMoreButtonKey,
+      this.padding});
 
   final Function onShowMore;
   final Future<T> future;
@@ -23,22 +23,23 @@ class InfoCard<T> extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-
     return FutureBuilder(
         future: future,
         builder: (final context, final snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-
             // this condition resolves to true if the snapshot contains some data and if that data is iterable and not empty
-            final bool toDisplayContent = snapshot.hasData
-                && !((snapshot.data is Map ||
-                      snapshot.data is Iterable) &&
-                      snapshot.data.isEmpty);
+            final bool toDisplayContent = snapshot.hasData &&
+                !((snapshot.data is Map || snapshot.data is Iterable) &&
+                    snapshot.data.isEmpty);
 
             // if there is no content to be displayed, we default to the chosen strategy for the no-items widget
             return toDisplayContent
-                ? cardWrapper(context: context, cardContent: builder(snapshot.data))
-                : (showIfEmpty ? cardWrapper(context: context, cardContent: noneYet(context)) : const SizedBox());
+                ? cardWrapper(
+                    context: context, cardContent: builder(snapshot.data))
+                : (showIfEmpty
+                    ? cardWrapper(
+                        context: context, cardContent: noneYet(context))
+                    : const SizedBox());
           }
           return const SizedBox(
             height: 100,
@@ -47,76 +48,66 @@ class InfoCard<T> extends StatelessWidget {
         });
   }
 
-  Widget cardWrapper({
-    @required final BuildContext context,
-    @required final Widget cardContent
-  }) => Padding(
-      padding: padding ?? const EdgeInsets.fromLTRB(12, 12, 12, 0),
-      child: Card(
-          child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                  children: <Widget>[
+  Widget cardWrapper(
+          {@required final BuildContext context,
+          @required final Widget cardContent}) =>
+      Padding(
+          padding: padding ?? const EdgeInsets.fromLTRB(12, 12, 12, 0),
+          child: Card(
+              child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(children: <Widget>[
                     cardHeader(context),
                     const SizedBox(height: 10),
                     cardContent,
-                  ]
-              )
-          )
-      )
-  );
+                  ]))));
 
-  Widget noneYet(
-      final BuildContext context) => SizedBox(
-    height: 100,
-    child: Center(
-      child: Text(
-        S.current.warningNoneYet,
-        style: TextStyle(color: Theme.of(context).disabledColor),
-      ),
-    ),
-  );
-
-  Widget cardHeader(
-      final BuildContext context
-      ) => Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-      if (title != null && title.isNotEmpty)
-        Expanded(
+  Widget noneYet(final BuildContext context) => SizedBox(
+        height: 100,
+        child: Center(
           child: Text(
-            title,
-            style: Theme.of(context)
-                .textTheme
-                .headline6
-                .copyWith(fontSize: 18),
-            overflow: TextOverflow.fade,
-            maxLines: 2,
+            S.current.warningNoneYet,
+            style: TextStyle(color: Theme.of(context).disabledColor),
           ),
         ),
-      if (onShowMore != null)
-        GestureDetector(
-          onTap: onShowMore,
-          key: showMoreButtonKey ?? const ValueKey('show_more'),
-          child: Row(
-            children: <Widget>[
-              Text(
-                S.current.actionShowMore,
+      );
+
+  Widget cardHeader(final BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          if (title != null && title.isNotEmpty)
+            Expanded(
+              child: Text(
+                title,
                 style: Theme.of(context)
-                    .coloredTextTheme
-                    .subtitle2
-                    .copyWith(
-                    color: Theme.of(context).primaryColor),
+                    .textTheme
+                    .headline6
+                    .copyWith(fontSize: 18),
+                overflow: TextOverflow.fade,
+                maxLines: 2,
               ),
-              Icon(
-                Icons.arrow_forward_ios_outlined,
-                color: Theme.of(context).primaryColor,
-                size:
-                Theme.of(context).textTheme.subtitle2.fontSize,
-              )
-            ],
-          ),
-        ),
-    ],
-  );
+            ),
+          if (onShowMore != null)
+            GestureDetector(
+              onTap: onShowMore,
+              key: showMoreButtonKey ?? const ValueKey('show_more'),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    S.current.actionShowMore,
+                    style: Theme.of(context)
+                        .coloredTextTheme
+                        .subtitle2
+                        .copyWith(color: Theme.of(context).primaryColor),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    color: Theme.of(context).primaryColor,
+                    size: Theme.of(context).textTheme.subtitle2.fontSize,
+                  )
+                ],
+              ),
+            ),
+        ],
+      );
 }
